47f3a21316d00a9d1d2466aef452f12ec179c8ee
noah-not-tj <noah.h.pickett@gmail.com>
2026-06-26 15:32:35 -0500

    自己做的比较好

--- 0-24.hs
+++ 0-24.hs
@@ -27,3 +27,27 @@ main :: IO()
 main = do
   let asdlfkj = pack "aaaabccaadeeee" 
   print asdlfkj 
+
+10 encode string 
+firstgroup :: Eq a => [a] -> ([a], [a])
+firstgroup (x : ys@(y : _)) 
+  | x == y = let (d, r) = firstgroup ys in (x : d, r)
+  | otherwise = ([x], ys)
+firstgroup lonely = (lonely, [])
+
+pack :: Eq a => [a] -> [[a]]
+pack []  = []
+pack xs = duplicates : pack remainder
+  where (duplicates, remainder) = firstgroup xs
+ 
+len :: [a] -> (Int, a)
+len xs@(x:_) = (length xs, x)
+  
+encode :: Eq a => [a] -> [(Int, a)]
+encode [] = []
+encode xs = map len $ pack xs
+  
+main :: IO() 
+main = do
+  let asdlfkj = encode "aaaabccaadeeee" 
+  print asdlfkj 
