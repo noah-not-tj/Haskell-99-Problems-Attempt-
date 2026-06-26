2747f1929cbac7dba88a08b5382e6d1786b4bd60
noah-not-tj <noah.h.pickett@gmail.com>
2026-06-26 15:26:04 -0500

    why are these still kinda tough?

--- 0-24.hs
+++ 0-24.hs
@@ -10,3 +10,20 @@ main :: IO()
 main = do
   let asdlfkj = compress "aaaabccaadeeee"
   print asdlfkj 
+
+9 compress / pack elements into list of lists
+firstgroup :: Eq a => [a] -> ([a], [a])
+firstgroup (x : ys@(y : _)) 
+  | x == y = let (d, r) = firstgroup ys in (x : d, r)
+  | otherwise = ([x], ys)
+firstgroup lonely = (lonely, [])
+
+pack :: Eq a => [a] -> [[a]]
+pack []  = []
+pack xs = duplicates : pack remainder
+  where (duplicates, remainder) = firstgroup xs
+  
+main :: IO() 
+main = do
+  let asdlfkj = pack "aaaabccaadeeee" 
+  print asdlfkj 
