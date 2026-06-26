c24c1b71a8723c8f20e54c3c79f387b23f668958
noah-not-tj <noah.h.pickett@gmail.com>
2026-06-26 15:44:17 -0500

    it is swell with my swoll

--- 0-24.hs
+++ 0-24.hs
@@ -51,3 +51,41 @@ main :: IO()
 main = do
   let asdlfkj = encode "aaaabccaadeeee" 
   print asdlfkj 
+
+10 modified run length encoding
+data Encoding a = Single a | Multiple Int a
+  deriving (Show)
+
+firstgroup :: Eq a => [a] -> ([a], [a])
+firstgroup (x : ys@(y : _)) 
+  | x == y = let (d, r) = firstgroup ys in (x : d, r)
+  | otherwise = ([x], ys)
+firstgroup lonely = (lonely, [])
+
+pack :: Eq a => [a] -> [[a]]
+pack []  = []
+pack xs = duplicates : pack remainder
+  where (duplicates, remainder) = firstgroup xs
+ 
+len :: [a] -> (Int, a)
+len xs@(x:_) = (length xs, x)
+  
+encode :: Eq a => [a] -> [(Int, a)]
+encode [] = []
+encode xs = map len $ pack xs
+
+encodeblah :: Eq a => (Int, a) -> Encoding a
+encodeblah (num, x) 
+  | num > 1 = Multiple num x
+  | otherwise = Single x
+
+encodeModified :: Eq a => [a] -> [Encoding a]
+encodeModified [] = []
+encodeModified xs = map encodeblah $ encode xs
+  
+main :: IO() 
+main = do
+  let asdlfkj = encodeModified "aaaabccaadeeee" 
+  print asdlfkj 
+  
+  
