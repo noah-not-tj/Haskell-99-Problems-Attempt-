a548a0e373f075801f3e5ec890b90e52f9fd2f9f
noah-not-tj <noah.h.pickett@gmail.com>
2026-06-29 16:07:24 -0500

    come back to this one for sure, not horrible but needed solution

--- 54-69.hs
+++ 54-69.hs
@@ -1,4 +1,25 @@
---55 and 56 completelybalancedtree and symmetric
+--55  had to look at solution though
+data Tree a = Empty | Branch a (Tree a) (Tree a)
+  deriving (Show)
+
+createBalancedTree :: Int -> [Tree ()]
+createBalancedTree 0 = [Empty]
+createBalancedTree n
+  | even n    = generate  (n `div` 2) ( (n `div` 2) - 1) 
+                  ++ generate ( (n `div` 2) - 1) (n `div` 2)
+                  
+  | otherwise = generate (n `div` 2) (n `div` 2)
+  
+generate :: Int -> Int -> [Tree ()]
+generate x y = [Branch () l r | l <- ltrees, r <- rtrees]
+  where ltrees = createBalancedTree x
+        rtrees = createBalancedTree y 
+
+main :: IO ()
+main = do
+  print $ createBalancedTree 3
+  
+--56 completelybalancedtree and symmetric
 data Tree a = Empty | Branch a (Tree a) (Tree a)
   deriving Show
 
